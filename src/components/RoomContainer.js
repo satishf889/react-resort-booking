99f7bb47e5f1071f4b07f709c2e144ec196f91a3
Satish Fulwani <satish.fulwani63@gmail.com>
2020-05-14 20:49:16 +0530

    Add filter by type of room and capacity of guests

---
+++ src/components/RoomContainer.js
@@ -0,0 +1,41 @@
+import React from "react";
+import RoomFilter from "./RoomFilter";
+import RoomList from "./RoomList";
+import { withRoomConsumer } from "../context";
+import Loading from "../components/Loading";
+
+function RoomsContainer({ context }) {
+  const { loading, sortedRooms, rooms } = context;
+  if (loading) {
+    return <Loading />;
+  }
+  return (
+    <>
+      <RoomFilter rooms={rooms} />
+      <RoomList rooms={sortedRooms} />
+    </>
+  );
+}
+
+export default withRoomConsumer(RoomsContainer);
+
+//One way of getting data in functional component
+// export default function RoomsContainer() {
+//   return (
+//     <RoomConsumer>
+//       {(value) => {
+//         const { loading, sortedRooms, rooms } = value;
+//         if (loading) {
+//           return <Loading />;
+//         }
+//         return (
+//           <div>
+//             Hello from Rooms Container
+//             <RoomFilter rooms= {rooms} />
+//             <RoomList rooms={sortedRooms} />
+//           </div>
+//         );
+//       }}
+//     </RoomConsumer>
+//   );
+// }
