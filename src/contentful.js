6fe023ef2a4fa5c362a25ddab53ca6caa0ddf372
Satish Fulwani <satish.fulwani63@gmail.com>
2020-05-15 18:48:53 +0530

    Add all files with final touch

--- src/contentful.js
+++ src/contentful.js
@@ -1,6 +1,5 @@
 import { createClient } from "contentful";
 
-console.log(`${JSON.stringify(process.env, null, 2)}`);
 export default createClient({
   space: process.env.REACT_APP_API_SPACE,
   accessToken: process.env.REACT_APP_ACCESS_TOKEN,
